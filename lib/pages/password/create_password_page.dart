f3b884d8e6b32173ac69eb62341e36f25a41e64a
GlagolSpirinS <112942625+GlagolSpirinS@users.noreply.github.com>
2025-01-07 05:28:26 +0300

    0.1.

---
+++ lib/pages/password/create_password_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/design/images.dart';
+
+import '../../design/colors.dart';
+
+class CreatePasswordPage extends StatelessWidget {
+  const CreatePasswordPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: firstColor,
+    ),
+    );
+  }}
